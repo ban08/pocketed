e956e753d5a208b23f06b60cd0569add1050aea3
jony <joao.sv.gusmao@gmail.com>
2026-03-10 17:58:00 +0000

    change shtuff

---
+++ mobile-app/mobile-app/src/theme/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  brand: '#FFAA00',
+  primary: '#FDF7EC',
+  secondary: '#6fe6be',
+  background: '#331832',
+  background_dark: '#1b001b',
+  textPrimary: '#FFFFFF',
+  textSecondary: '#AAAAAA',
+  danger: '#EF4444',
+  textDark: '#1b001b',
+  red: '#FF0000',
+  green: '#00FF00',
+  blue: '#0000FF',
+  hero: '#ffff00'
+};
